97122ef1fd0cbac314edd7716aaf2c5257ca6d8d
Ella Jameson <ellagjameson@gmail.com>
2020-09-29 10:27:51 -0700

    Remove makefile, add requirements to setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,16 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+requirements = []
+with open('requirements.txt', 'r') as fh:
+    for line in fh:
+        pkg = line.strip()
+        if pkg != "":
+            requirements.append(pkg)
+
 setuptools.setup(
     name="imgtag",
-    version="0.1.0",
+    version="0.1.2",
     author="Ella Jameson",
     author_email="ellagjamson@gmail.com",
     description="Simple XMP Image Tag & Metadata Editing Library",
@@ -19,4 +26,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    install_requires=requirements
 )
