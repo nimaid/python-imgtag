b25fb9078c7c0011ccca98b3e948f4cc276bcf81
Ella Jameson <ellagjameson@gmail.com>
2020-09-28 17:39:03 -0700

    First testing

---
+++ DEV.py
@@ -0,0 +1,113 @@
+import libxmp
+import warnings
+
+class ImgTag:
+    def __init__(self, filename, force_case=None, strip=True, no_duplicates=True):
+        self.filename = filename
+        self.is_open = False
+        
+        self.tags = None;
+        
+        self.open()
+        
+        # Add valueerrors
+        self.force_case = force_case
+        self.strip = strip
+        self.no_duplicates = no_duplicates
+    
+    def __del__(self):
+        self.close()
+    
+    def open(self):
+        if not self.is_open:
+            # Open file for updating
+            self.xmpfile = libxmp.XMPFiles(file_path=self.filename, open_forupdate=True)
+            
+            # Try to read existing XMP
+            self.xmp = self.xmpfile.get_xmp()
+            
+            # Make new XMP if not exist
+            if self.xmp is None:
+                self.xmp = libxmp.core.XMPMeta()
+            
+            self.is_open = True
+
+    def close(self):
+        if self.is_open:
+            # Test if can write tags
+            if self.xmpfile.can_put_xmp(self.xmp):
+                # Write tags
+                self.xmpfile.put_xmp(self.xmp)
+                
+                # Close file
+                self.xmpfile.close_file()
+                saved = True
+            else:
+                # Close file
+                xmpfile.close_file()
+                warnings.warn("Could not save tags in image!")
+                saved = False
+            
+            self.is_open = False
+            return saved
+    
+    def get_tags(self):
+        # Open if not opened
+        if not self.is_open:
+            self.open()
+        
+        # Get existing tags
+        num_tags = self.xmp.count_array_items(libxmp.consts.XMP_NS_DC, "subject")
+        tags = [self.xmp.get_array_item(libxmp.consts.XMP_NS_DC, "subject", i).strip().lower() for i in range(1, num_tags+1)]
+        
+        # Strip
+        if strip == True:
+             tags = [x.strip() for x in tags]
+        elif strip != False:
+            raise ValueError("strip argument must be either True or False")
+        
+        # Force case
+        if force_case != None:
+            if force_case.lower() == "upper":
+                tags = [x.upper() for x in tags]
+            elif force_case.lower() == "lower":
+                tags = [x.lower() for x in tags]
+            else:
+                raise ValueError("force_case argument must be either None, 'upper' or 'lower'")
+        
+        # Remove duplicates
+        if no_duplicates == True:
+            self.tags = list(set(self.tags))
+        elif no_duplicates != False:
+            raise ValueError("no_duplicates argument must be either True or False")
+        
+        self.tags = tags
+        return self.tags
+    
+    def add_tags(self, tags):
+        # Get existing tags (updates self.tags)
+        temp = get_tags()
+        
+        # Strip
+        if strip == True:
+            tags = [x.strip() for x in tags]
+        
+        # Force case
+        if force_case != None:
+            if force_case.lower() == "upper":
+                tags = [x.upper() for x in tags]
+            elif force_case.lower() == "lower":
+                tags = [x.lower() for x in tags]
+        
+        # Append tags
+        for tag in tags:
+            # Skip duplicates
+            if no_duplicates:
+                if tag in self.tags:
+                    continue
+            
+            # Append
+            xmp.append_array_item(libxmp.consts.XMP_NS_DC, "subject", tag, {"prop_array_is_ordered":True, "prop_value_is_array":True})
+            self.tags.append(tag)
+    
+    
