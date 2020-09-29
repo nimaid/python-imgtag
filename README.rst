4559ba3a992f5cf401a4ae1a55d971bae6d566d3
Ella Jameson <ellagjameson@gmail.com>
2020-09-28 20:21:24 -0700

    initial changes

--- README.rst
+++ README.rst
@@ -1,10 +1,8 @@
-Sample Module Repository
-========================
+Python imgtag library
+======================
 
-This simple project is an example repo for Python projects.
-
-`Learn more <http://www.kennethreitz.org/essays/repository-structure-and-python>`_.
+Simple XMP Image Tag & Metadata Editing Library
 
 ---------------
 
-If you want to learn more about ``setup.py`` files, check out `this repository <https://github.com/kennethreitz/setup.py>`_.
+WIP
--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Learn more: https://github.com/kennethreitz/setup.py
-
 from setuptools import setup, find_packages
 
 
@@ -12,13 +10,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
+    name='imgtag',
     version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    description='Simple XMP Image Tag & Metadata Editing Library',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    author='Ella Jameson (nimaid)',
+    author_email='ellagjameson@gmail.com',
+    url='https://github.com/nimaid/python-imgtag',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
