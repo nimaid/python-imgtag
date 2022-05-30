6bed556d7e6235e9a93c974c74bba13643bdd86e
nimaid <ellagjameson@gmail.com>
2022-05-30 08:17:48 -0700

    fixes

--- imgtag/__init__.py
+++ imgtag/__init__.py
@@ -1,4 +1,3 @@
 from .core import (__DEFAULT_MEMORY_LIMIT_RATIO__,
                    __DEFAULT_BLOCK_SIZE__,
-                   ImgTag,
-                   imgtag.__DEFAULT_MEMORY_LIMIT_RATIO__ as __DEFAULT_MEMORY_LIMIT_RATIO__)
+                   ImgTag)
